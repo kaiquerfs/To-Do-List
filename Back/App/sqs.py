57e3161272df5c2f9a01b5799be18e15fadd1821
kaiquerfs <118867809+kaiquerfs@users.noreply.github.com>
2024-06-27 17:16:14 -0300

    Atualização das config. da fila - não finalizado

--- Back/App/sqs.py
+++ Back/App/sqs.py
@@ -2,8 +2,8 @@ import boto3
 import os
 import json
 
-sqs = boto3.client('sqs', region_name='us-east-1')  # Substitua pelo seu AWS_REGION
-queue_url = os.getenv('SQS_QUEUE_URL')
+sqs = boto3.client('sqs', region_name='Colocar a região da minha fila')  
+queue_url = os.getenv('Colocar minha URL da fila aqui')
 
 def enviar_mensagem_sqs(event_type, data):
     message = {
