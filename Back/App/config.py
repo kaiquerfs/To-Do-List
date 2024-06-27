143f891466222288801c6f757e3df66cf639d66b
kaiquerfs <118867809+kaiquerfs@users.noreply.github.com>
2024-06-27 17:38:03 -0300

    Update config.py

--- Back/App/config.py
+++ Back/App/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    AWS_REGION = os.getenv('AWS_REGION', 'us-east-1')
+    AWS_REGION = os.getenv('AWS_REGION', 'Minha região')
     S3_BUCKET = os.getenv('S3_BUCKET')
-    SQS_QUEUE_URL = os.getenv('SQS_QUEUE_URL')
+    SQS_QUEUE_URL = os.getenv('Url da Fila')
     DYNAMODB_TABLE = os.getenv('DYNAMODB_TABLE', 'todo_tasks')
